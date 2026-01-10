2d028da58357c33fdd837b4f7a029b6831afbfdb
AJAY7205 <ajaylogeswaran@gmail.com>
2026-01-10 06:41:19 +0530

    Initial commit

---
+++ Backend/ytrep/src/main/java/com/learning/ytrep/YtrepApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ytrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtrepApplication.class, args);
+	}
+
+}
