ffe75f06a37f6894f81e0d7a3d86efa8f7acf30c
AJAY7205 <ajaylogeswaran@gmail.com>
2026-08-12 18:26:26 +0530

    For the next trick You will see I die

--- Frontend/yourep-react/src/services/api/axios.config.js
+++ Frontend/yourep-react/src/services/api/axios.config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import axiosRetry from 'axios-retry';
 
 const API_BASE_URL = 'https://yourep-api.austriaeast.cloudapp.azure.com/api' || 'http://localhost:8080/api';
-
+console.log('API_BASE_URL:', API_BASE_URL); // Log the API base URL for debugging
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
